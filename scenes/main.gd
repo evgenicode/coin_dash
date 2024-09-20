4b39951f81974e078dfda833172ad8a9bf49f39a
evgenicode <83199154+evgenicode@users.noreply.github.com>
2024-09-20 11:04:18 +0300

    add ui, scripts

--- scenes/main.gd
+++ scenes/main.gd
@@ -13,7 +13,7 @@ func _ready():
 	screensize = get_viewport().get_visible_rect().size
 	$Player.screensize = screensize
 	$Player.hide()
-	new_game()
+	
 	
 func new_game():
 	playing = true
@@ -24,6 +24,8 @@ func new_game():
 	$Player.show()
 	$GameTimer.start()
 	spawn_coins()
+	$HUD.update_score(score)
+	$HUD.update_timer(time_left)
 
 func spawn_coins():
 	for i in level + 4:
@@ -31,3 +33,38 @@ func spawn_coins():
 		add_child(c)
 		c.screensize = screensize
 		c.position = Vector2(randi_range(0, screensize.x), randi_range(0, screensize.y))
+
+func _process(delta):
+	if playing and get_tree().get_nodes_in_group("coins").size() == 0:
+		level += 1
+		time_left += 5
+		spawn_coins()
+
+
+func _on_game_timer_timeout():
+	time_left -= 1
+	$HUD.update_timer(time_left)
+	if time_left <= 0:
+		game_over()
+
+
+func _on_player_hurt():
+	game_over()
+	
+	
+
+
+func _on_player_pickup():
+	score += 1
+	$HUD.update_score(score)
+	
+func game_over():
+	playing = false
+	$GameTimer.stop()
+	get_tree().call_group("coins", "queue_free")
+	$HUD.show_game_over()
+	$Player.die()
+
+
+func _on_hud_start_game():
+	new_game()
