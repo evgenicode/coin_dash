d2054db28886f2b81a0756106a3652de0470466e
evgenicode <83199154+evgenicode@users.noreply.github.com>
2024-09-22 16:57:11 +0300

    add powerup node

--- scenes/main.gd
+++ scenes/main.gd
@@ -2,6 +2,7 @@ extends Node
 
 @export var coin_scene: PackedScene
 @export var playtime = 30
+@export var powerup_scene : PackedScene
 
 var level = 1
 var score = 0
@@ -44,6 +45,8 @@ func _process(delta):
 		level += 1
 		time_left += 5
 		spawn_coins()
+		$PowerupTimer.wait_time = randf_range(5, 10)
+		$PowerupTimer.start()
 
 
 func _on_game_timer_timeout():
@@ -57,10 +60,17 @@ func _on_player_hurt():
 	game_over()
 
 
-func _on_player_pickup():
-	score += 1
-	$HUD.update_score(score)
-	$CoinSound.play()
+func _on_player_pickup(type):
+	match type:
+		"coin":
+			$CoinSound.play()
+			score += 1
+			$HUD.update_score(score)
+		"powerup":
+			$PowerupSound.play()
+			time_left += 5
+			$HUD.update_timer(time_left)
+	
 	
 func game_over():
 	playing = false
@@ -73,3 +83,10 @@ func game_over():
 
 func _on_hud_start_game():
 	new_game()
+
+
+func _on_powerup_timer_timeout():
+	var p = powerup_scene.instantiate()
+	add_child(p)
+	p.screensize = screensize
+	p.position = Vector2(randi_range(0, screensize.x), randi_range(0, screensize.y))
